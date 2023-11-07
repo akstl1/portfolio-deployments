b688c54426096d0f0ac1eab0a3ec7c2b4eeb562f
Allan Khariton <allankhariton@gmail.com>
2023-11-06 23:43:40 -0500

    added pokedex base file

--- Welcome.py
+++ Welcome.py
@@ -9,9 +9,11 @@ show_pages(
         Page("Welcome.py", "Home", "🏠"),
         Section("Additional Projects", icon="🎈️"),
         Page("pages/analyducks.py", "Analyducks"),
+        # Page("pages/pokedex.py", "Pokedex"),
         # # Pages after a section will be indented
         # Page("Another page", icon="💪"),
         # # Unless you explicitly say in_section=False
         # Page("Not in a section", in_section=False)
     ]
   )
+
