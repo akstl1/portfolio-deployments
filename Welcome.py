543417f691465b862e22e48bc44bf0728f051eec
Allan Khariton <allankhariton@gmail.com>
2024-01-30 22:13:46 -0500

    updating welcome styling

--- Welcome.py
+++ Welcome.py
@@ -21,7 +21,27 @@ with head2:
     st.header("Data Science Portfolio")
     st.write("I'm a Data Analyst II, and advancing in my career as a data professional. My portfolio focuses on interesting projects I've recently undertaken, with a strong emphasis on business impact and learning new tools & languages. You can view my projects in the posts below, and visit my Github & LinkedIn pages (or download my Resume) by using the links below.")
 # Specify what pages should be shown in the sidebar, and what their titles and icons
-# should be
+
+st.write("##")
+link1,link2,link3,link4 = st.columns([.25,.25,.25,.25])
+with link1:
+    st.write("Rockville, MD")
+with link2:
+    url2 = "https://github.com/akstl1"
+    st.write("[Github](%s)" % url2)
+
+with link3:
+    url3 = "https://www.linkedin.com/in/allan-khariton/"
+    # st.write("[![Title](<https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg>)](<https://www.linkedin.com/in/allan-khariton/>)" "[LinkedIn](%s)" % url3)
+    st.write("[LinkedIn](%s)" % url3)
+
+with link4:
+    url4 = "https://akstl1.github.io/docs/A_Khariton_Resume_2022.08.22.pdf"
+    
+    st.write( "[Resume](%s)" % url4)
+
+    # st.markdown("[![Title](<https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg>)](<https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg>)")
+
 show_pages(
     [
         Page("Welcome.py", "Home", "🏠"),
